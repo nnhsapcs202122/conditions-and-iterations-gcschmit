76da14f9adb6379f13ca1b24be87cecfe4c2ad24
Geoff Schmit <gcschmit@gmail.com>
2021-11-04 09:34:02 -0500

    add while loop and for loop examples

--- Iterations.java
+++ Iterations.java
@@ -9,5 +9,117 @@ import java.util.Scanner;
  */
 public class Iterations
 {
+    public static void whileExample()
+    {
+        /*
+         * while loop:
+         * 
+         *      evaluates a boolean expression (i.e., condition)
+         *          if true, executes the body of the loop and then re-evaluates the condition
+         *          if false, skips the body and continues
+         */
+        int count = 1;                                  // initialization
+        
+        while(count <= 5)                               // condition
+        {
+            System.out.println(count);                  // body
+            
+            count++;                                    // update the loop variable
+        }
+        
+        System.out.println("done");
+    }
     
+    public static void whileExample2()
+    {
+        /*
+         * This is an example of an infinite loop!
+         * 
+         *  A better condition would be:
+         *      while(count < 50)
+         */
+        int count = 1;                                  // initialization
+        
+        while(count != 50)                              // condition
+        {
+            System.out.println(count);                  // body
+            
+            count += 2;                                 // update the loop variable
+        }
+        
+        System.out.println("done");
+    }
+    
+    public static void forExample()
+    {
+        /*
+         * for loop:
+         * 
+         *      Three parts of the for loop statement:
+         *          1. initialization - executed once
+         *          2. condition - boolean expression evaluated at the start of each iteration
+         *          3. update the loop variable - executed at the *end* of each iteration
+         *                                          *before* evaluating the condition again
+         */
+        for(int count = 1;                      // initialization
+                count <= 5;                     // condition
+                count++)                        // update the loop variable
+        {
+            System.out.println(count);          // body
+        }
+        
+        System.out.println("done");
+        
+        /*
+         * variables declared within the for statement are scoped to the for statement
+         *      and its body
+         */
+        //System.out.println("final value of count: " + count);
+    }
+    
+    public static void forExample2()
+    {
+        int count;
+        for(count = 1;                          // initialization
+                count <= 5;                     // condition
+                count++)                        // update the loop variable
+        {
+            System.out.println(count);          // body
+        }
+        
+        System.out.println("done");
+        
+        System.out.println("final value of count: " + count);
+    }
+    
+    public static void offByOne()
+    {
+        /*
+         * The infamous off-by-one error is common with for loops
+         *      executing one too many or one too few times.
+         *      
+         *  Carefully ask:  should the initial value start at 0 or 1?
+         *                  should the condition be < or <=?
+         *                  
+         *      "Think! Don't compile and try at random!"
+         *      
+         *  By convention, for simple for loops we start at 0 and we use <
+         */
+        
+        // we want to print five "*"
+        for(int i = 0;                          // initialization
+                i <= 5; // should be i < 5      // condition
+                i++)                            // update the loop variable
+        {
+            System.out.println("*");            // body
+        }
+        
+        System.out.println("done");
+    }
 }
+
+
+
+
+
+
